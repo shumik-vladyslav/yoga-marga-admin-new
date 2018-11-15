f95c00c3d1185bb937bf318213d20ff74aa79e08
shumik-vladyslav <shumik.vladyslav@gmail.com>
2018-11-15 23:22:30 +0200

    MenuLayout component init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { routes } from './app.routes';
 import { RouterModule } from '@angular/router';
+import { MenuLayoutComponent } from './components/menu-layout/menu-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuLayoutComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
