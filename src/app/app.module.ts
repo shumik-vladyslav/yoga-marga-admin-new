237a4b57f6b5c3d30d627b21bbc4708712bd68a5
shumik-vladyslav <shumik.vladyslav@gmail.com>
2018-11-16 00:07:25 +0200

    Sign-in init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { routes } from "./app.routes";
 import { RouterModule } from "@angular/router";
 import { MenuLayoutComponent } from "./components/menu-layout/menu-layout.component";
 import { AccountsComponent } from "./components/accounts/accounts.component";
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuLayoutComponent, AccountsComponent],
+  declarations: [AppComponent, MenuLayoutComponent, AccountsComponent, SignInComponent],
   imports: [RouterModule.forRoot(routes, { useHash: true }), BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { AccountsComponent } from './components/accounts/accounts.component';
 import { Component } from '@angular/core';
 import { MenuLayoutComponent } from "./components/menu-layout/menu-layout.component";
 import { Routes } from "@angular/router";
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 export const routes: Routes = [
   { path: "", component: MenuLayoutComponent, children: [
@@ -13,5 +14,9 @@ export const routes: Routes = [
         path: "",
         component: AccountsComponent
     },
-  ] }
+  ] },
+  {
+      path: "sign-in",
+      component: SignInComponent
+  }
 ];
